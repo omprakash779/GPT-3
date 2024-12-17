5fc094b1b34f0823ac1752f0952dd9b44791d6aa
omprakash <sahooomprakash091@gmail.com>
2024-12-17 08:43:53 +0530

    Modern ChatGPT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {Brand, Cta, Navbar } from './Components';
+import { Features,Blog, Footer, Header, Possibility,WhatGPT3 } from './Containers';
 
-function App() {
+const App =() => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <h1>GPT-3</h1> */}
+      <Navbar />
+      <Header />
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility /> 
+      <Cta />
+      <Blog />
+      <Footer />
+      
     </div>
   );
 }
